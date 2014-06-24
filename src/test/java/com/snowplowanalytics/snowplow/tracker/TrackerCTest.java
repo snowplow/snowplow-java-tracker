24a0fbce10222c1b117d928e6026a8f8034a5d31
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-06-24 00:29:30 -0400

    All TrackerC tests separated  - Fixes #5

--- src/test/java/com/snowplowanalytics/snowplow/tracker/TrackerCTest.java
+++ src/test/java/com/snowplowanalytics/snowplow/tracker/TrackerCTest.java
@@ -14,71 +14,44 @@ import java.util.Map;
 
 public class TrackerCTest extends TestCase {
 
-    @Before
-    public void setUp() throws Exception {
-//        Tracker t1 = new TrackerC("d3rkrsqld9gmqf.cloudfront.net", "com.snowplowanalytics.snowplow.tracker.Tracker Test", "JavaPlow", "com.saggezza", true, true);
-////        t1.track();
-//        t1.setUserID("User1");
-//        t1.setLanguage("ital");
-//        t1.setPlatform("mob");
-//        t1.setScreenResolution(760, 610);
-//        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
-//
-//        ///// E COMMERCE TEST
-//        Map<String,String> items = new HashMap<String, String>();
-//        items.put("sku", "SKUVAL"); items.put("quantity","2"); items.put("price","19.99");
-//        List<Map<String,String>> lst = new LinkedList<Map<String, String>>();
-//        lst.add(items);
-//        TrackerC.debug=true;
-//
-//        /////TRACK TEST
-//        for (int i = 0; i < 2; i++) {
-//            t1.track();
-//            try { Thread.sleep(2000); }
-//            catch (InterruptedException e){}
-//            System.out.println("Loop " + i);
-//            Map<String, Object> dict = new LinkedHashMap<String, Object>();
-//            dict.put("Iteration", i);
-//            t1.trackUnstructEvent("Lube Insights", "Data Loop", dict, context);
-//            t1.trackStructEvent("Items", "Stuff", "Pants", "Green Blue", 3, DEFAULT_VENDOR, context);
-//            t1.trackPageView("www.saggezza.com", "Saggezza Home", "KG", null);
-//            t1.trackEcommerceTransactionItem("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes","USD",null,null);
-//            t1.trackEcommerceTransaction("OID", 19.99, "Kohls", 2.50, 1.99, "Chagrin", "OH", "USA", "USD", lst, context);
-//        }
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
     @Test
     public void testTrackEcommerceTransactionItem() throws Exception {
-//        Tracker t1 = new TrackerC("d3rkrsqld9gmqf.cloudfront.net", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
-        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+//        Tracker t1 = new TrackerC("d3rkrsqld9gmqf.cloudfront.net",
+//                "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker",
+                "JavaPlow", "com.snowplow", true, true);
         t1.setUserID("User1");
         t1.setLanguage("ital");
         t1.setPlatform("mob");
         t1.setScreenResolution(760, 610);
         String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
-        t1.trackEcommerceTransactionItem("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes", "USD", null, context);
+
+        t1.trackEcommerceTransactionItem("IT1023", "SKUVAL",
+                29.99, 2, "boots",
+                "Shoes", "USD",
+                null, context);
     }
 
     @Test
     public void testTrackEcommerceTransactionItem2() throws Exception {
-        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker",
+                "JavaPlow", "com.snowplow", true, true);
         t1.setUserID("User1");
         t1.setLanguage("ital");
         t1.setPlatform("mob");
         t1.setScreenResolution(760, 610);
         String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
-        t1.trackEcommerceTransactionItem("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes", "USD", null, null);
+
+        t1.trackEcommerceTransactionItem("IT1023", "SKUVAL",
+                29.99, 2, "boots",
+                "Shoes", "USD",
+                null, null);
     }
 
     @Test
     public void testTrackEcommerceTransaction() throws Exception {
-        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker",
+                "JavaPlow", "com.snowplow", true, true);
         t1.setUserID("User1");
         t1.setLanguage("ital");
         t1.setPlatform("mob");
@@ -90,12 +63,17 @@ public class TrackerCTest extends TestCase {
         List<Map<String,String>> lst = new LinkedList<Map<String, String>>();
         lst.add(items);
 
-        t1.trackEcommerceTransaction("OID", 19.99, "Kohls", 2.50, 1.99, "Chagrin", "OH", "USA", "USD", lst, context);
+        t1.trackEcommerceTransaction("OID", 19.99,
+                "Kohls", 2.50,
+                1.99, "Chagrin",
+                "OH", "USA",
+                "USD", lst, context);
     }
 
     @Test
     public void testTrackUnstructEvent() throws Exception {
-        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker",
+                "JavaPlow", "com.snowplow", true, true);
         t1.setUserID("User1");
         t1.setLanguage("ital");
         t1.setPlatform("mob");
@@ -106,5 +84,30 @@ public class TrackerCTest extends TestCase {
         dict.put("Iteration", 1);
 
         t1.trackUnstructEvent("Lube Insights", "Data Loop", dict, context);
+    }
+
+    @Test
+    public void testTrackStructEvent() throws Exception {
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker",
+                "JavaPlow", "com.snowplow", true, true);
+        t1.setUserID("User1");
+        t1.setLanguage("ital");
+        t1.setPlatform("mob");
+        t1.setScreenResolution(760, 610);
+        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
+        t1.trackStructEvent("Items", "Stuff", "Pants", "Green Blue", 3, "com.snowplow", context);
+    }
+
+    @Test
+    public void testTrackPageView() throws Exception {
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker",
+                "JavaPlow", "com.snowplow", true, true);
+        t1.setUserID("User1");
+        t1.setLanguage("ital");
+        t1.setPlatform("mob");
+        t1.setScreenResolution(760, 610);
+        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
+
+        t1.trackPageView("www.saggezza.com", "Saggezza Home", "KG", null);
     }
 }
