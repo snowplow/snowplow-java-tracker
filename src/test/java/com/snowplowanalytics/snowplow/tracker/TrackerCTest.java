b2bc23b5bc9a72125bbe025d05010c272414696f
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-06-23 23:55:09 -0400

    Moved some few tests from main to unit test class

--- src/test/java/com/snowplowanalytics/snowplow/tracker/TrackerCTest.java
+++ src/test/java/com/snowplowanalytics/snowplow/tracker/TrackerCTest.java
@@ -12,8 +12,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
-
 public class TrackerCTest extends TestCase {
 
     @Before
@@ -41,11 +39,11 @@ public class TrackerCTest extends TestCase {
 //            System.out.println("Loop " + i);
 //            Map<String, Object> dict = new LinkedHashMap<String, Object>();
 //            dict.put("Iteration", i);
-//            t1.track_unstruct_event("Lube Insights", "Data Loop", dict, context);
-//            t1.track_struct_event("Items", "Stuff", "Pants", "Green Blue", 3, DEFAULT_VENDOR, context);
-//            t1.track_page_view("www.saggezza.com", "Saggezza Home", "KG", null);
-//            t1.track_ecommerce_transaction_item("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes","USD",null,null);
-//            t1.track_ecommerce_transaction("OID", 19.99, "Kohls", 2.50, 1.99, "Chagrin", "OH", "USA", "USD", lst, context);
+//            t1.trackUnstructEvent("Lube Insights", "Data Loop", dict, context);
+//            t1.trackStructEvent("Items", "Stuff", "Pants", "Green Blue", 3, DEFAULT_VENDOR, context);
+//            t1.trackPageView("www.saggezza.com", "Saggezza Home", "KG", null);
+//            t1.trackEcommerceTransactionItem("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes","USD",null,null);
+//            t1.trackEcommerceTransaction("OID", 19.99, "Kohls", 2.50, 1.99, "Chagrin", "OH", "USA", "USD", lst, context);
 //        }
 
     }
@@ -57,12 +55,57 @@ public class TrackerCTest extends TestCase {
 
     @Test
     public void testTrack_ecommerce_transaction_item() throws Exception {
-        Tracker t1 = new TrackerC("segfault.ngrok.com", "my_namespace", "my_appid", "context_vendor", false, false);
-        t1.track();
+//        Tracker t1 = new TrackerC("d3rkrsqld9gmqf.cloudfront.net", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        t1.setUserID("User1");
+        t1.setLanguage("ital");
+        t1.setPlatform("mob");
+        t1.setScreenResolution(760, 610);
+        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
+        t1.trackEcommerceTransactionItem("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes", "USD", null, context);
+    }
+
+    @Test
+    public void testTrack_ecommerce_transaction_item2() throws Exception {
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        t1.setUserID("User1");
+        t1.setLanguage("ital");
+        t1.setPlatform("mob");
+        t1.setScreenResolution(760, 610);
+        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
+        t1.trackEcommerceTransactionItem("IT1023", "SKUVAL", 29.99, 2, "boots", "Shoes", "USD", null, null);
     }
 
     @Test
     public void testTrack_ecommerce_transaction() throws Exception {
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        t1.setUserID("User1");
+        t1.setLanguage("ital");
+        t1.setPlatform("mob");
+        t1.setScreenResolution(760, 610);
+        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
 
+        // TODO: This bit needs to be changed to accept a list of ecomm transaction items
+        Map<String,String> items = new HashMap<String, String>();
+        items.put("sku", "SKUVAL"); items.put("quantity","2"); items.put("price","19.99");
+        List<Map<String,String>> lst = new LinkedList<Map<String, String>>();
+        lst.add(items);
+
+        t1.trackEcommerceTransaction("OID", 19.99, "Kohls", 2.50, 1.99, "Chagrin", "OH", "USA", "USD", lst, context);
+    }
+
+    @Test
+    public void testTrack_unstruct_event() throws Exception {
+        Tracker t1 = new TrackerC("segfault.ngrok.com", "com.snowplowanalytics.snowplow.tracker", "JavaPlow", "com.snowplow", true, true);
+        t1.setUserID("User1");
+        t1.setLanguage("ital");
+        t1.setPlatform("mob");
+        t1.setScreenResolution(760, 610);
+        String context = "{'Zone':'USA', 'Phone':'Droid', 'Time':'2pm'}";
+
+        Map<String, Object> dict = new LinkedHashMap<String, Object>();
+        dict.put("Iteration", 1);
+
+        t1.trackUnstructEvent("Lube Insights", "Data Loop", dict, context);
     }
 }
