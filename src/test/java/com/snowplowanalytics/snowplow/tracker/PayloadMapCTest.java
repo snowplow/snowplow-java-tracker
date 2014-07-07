a034c42edaa26ca40ff75d16703dc7970d4e9089
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-07-07 05:13:30 -0400

    Initial PayloadMapC tests
    - Incomplete for #34

--- src/test/java/com/snowplowanalytics/snowplow/tracker/PayloadMapCTest.java
+++ src/test/java/com/snowplowanalytics/snowplow/tracker/PayloadMapCTest.java
@@ -4,15 +4,52 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import java.util.LinkedHashMap;
-import java.util.Set;
-
-import static org.junit.Assert.*;
-
 public class PayloadMapCTest extends TestCase {
 
     @Test
-    public void testEverything() throws Exception {
-        // Make tests
+    public void testEmptyParamConstructor() throws Exception {
+        PayloadMap payloadMap = new PayloadMapC();
+        assertEquals(true, payloadMap.getParams().isEmpty());
     }
+
+    @Test
+    public void testEmptyConfigConstructor() throws Exception {
+        PayloadMap payloadMap = new PayloadMapC();
+        assertEquals(true, payloadMap.getConfigs().isEmpty());
+    }
+
+    @Test
+    public void testSetParamMap() throws Exception {
+        LinkedHashMap<String, String> param = new LinkedHashMap<String, String>();
+        param.put("e","pv");
+        PayloadMap payloadMap = new PayloadMapC(param,null);
+        assertSame(param, payloadMap.getParams());
+//        assertNull(payloadMap.getConfigs());
+    }
+
+    @Test
+    public void testSetTransactionId() throws Exception {
+        PayloadMapC payloadMap = new PayloadMapC();
+        payloadMap.setTransactionID();
+        Integer i = Integer.parseInt(payloadMap.getParam("tid"));
+        assertTrue(i instanceof Integer);
+    }
+
+    @Test
+    public void testSetTimestamp() throws Exception {
+        PayloadMapC payloadMap = new PayloadMapC();
+        payloadMap.setTimestamp();
+        Long i = Long.parseLong(payloadMap.getParam("dtm"));
+        assertTrue(i instanceof Long);
+    }
+
+//    @Test
+//    public void testSetTimestamp2() throws Exception {
+//        PayloadMapC payloadMap = new PayloadMapC();
+//        payloadMap.setTimestamp(123456L);
+//        Long i = Long.parseLong(payloadMap.getParam("dtm"));
+//        Long j = 123456L;
+//        assertSame(j, i);
+//    }
+
 }
