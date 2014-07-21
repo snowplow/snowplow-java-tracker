bc23cc2bf8786168684710b725cb83736147e140
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-07-21 12:17:32 -0400

    Generates Version class from gradle automagically
    - A reply from the gradle forum gave the solution to write the file as
      the task 'generateSources' to update the Version class with the gradle
      version.
    - Fixes #13

--- src/main/java/com/snowplowanalytics/snowplow/tracker/Version.java
+++ src/main/java/com/snowplowanalytics/snowplow/tracker/Version.java
@@ -13,14 +13,6 @@
 
 package com.snowplowanalytics.snowplow.tracker;
 
-/**
- * The version class is used for documentation purposes.
- * It follows the python convention of creating a version identity class
- *
- * @version 0.4.0
- * @author Kevin Gleason
- */
-
 public class Version {
     static final String VERSION = "0.4.0";
 }
