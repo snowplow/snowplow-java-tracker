9a5115957ba4104d11def073c00ccdd364dec9aa
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-07-22 15:19:20 -0400

    Empty Subject test class

---
+++ src/test/java/com/snowplowanalytics/snowplow/tracker/SubjectTest.java
@@ -0,0 +1,48 @@
+package com.snowplowanalytics.snowplow.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubjectTest {
+
+    @Test
+    public void testSetPlatform() throws Exception {
+
+    }
+
+    @Test
+    public void testSetUserId() throws Exception {
+
+    }
+
+    @Test
+    public void testSetScreenResolution() throws Exception {
+
+    }
+
+    @Test
+    public void testSetViewPort() throws Exception {
+
+    }
+
+    @Test
+    public void testSetColorDepth() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTimezone() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLanguage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSubject() throws Exception {
+
+    }
+}
